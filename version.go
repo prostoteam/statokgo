fa3bb9686a1b5384514e57333d36fe60c90696c2
en <godknowsiamgood@gmail.com>
2026-01-10 23:10:28 +0300

    ver

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package statok
 
 // VersionString is the human-readable client version. Update this manually.
-const VersionString = "20260110-001"
+const VersionString = "20260110-003"
 
 // Version returns the client version string.
 func Version() string {
